da21e4a8d2c16d1c7da82521fe850425ef9dfdb7
727721euit096-NALLASIVAM <125950986+727721euit096-NALLASIVAM@users.noreply.github.com>
2023-04-13 12:41:29 +0530

    Add files via upload

---
+++ cc-1/src/test/java/CIA1/cc_1/AppTest.java
@@ -0,0 +1,20 @@
+package CIA1.cc_1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
